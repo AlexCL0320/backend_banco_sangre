7dda51b643c5566e6e0eb372d5a45e5df1bc1f58
AlexCL0320 <lopez.oswaldo.1d@gmail.com>
2025-06-04 19:05:08 -0600

    Preparación Despliegue2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # build.sh
-
+pip install -r requirements.txt
 # Aplicar migraciones y recolectar archivos estáticos
 python manage.py migrate
 python manage.py collectstatic --noinput
